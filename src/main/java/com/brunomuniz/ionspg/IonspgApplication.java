c1aa3bbbe8ea271f4176af2d185dbf3574ef19ed
Bruno Muniz <brunomunizdev@gmail.com>
2018-11-23 22:18:45 -0200

    Projeto foi criado

---
+++ src/main/java/com/brunomuniz/ionspg/IonspgApplication.java
@@ -0,0 +1,12 @@
+package com.brunomuniz.ionspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonspgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonspgApplication.class, args);
+	}
+}
